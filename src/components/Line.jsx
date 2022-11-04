315084722518dbd4b29432ed92575b0f64c23085
Deborah Alves <deborah.alves@gmail.com>
2022-11-04 14:02:47 -0300

    resolvendo os amarelo

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,11 +1,48 @@
 import Square from "./Square";
 
 function Line({ isActive, selectedSquare, letters, done, word, handleSquareClick }) {
+    let countYellows = (word, letters, l) => {
+      let greens = 0;
+      let countInWord = 0;
+      let countInLetters = 0;
+      for (let i = 0; i < word.length; i++) {
+        if (word.charAt(i) === l && letters[i].toLowerCase() === l) {
+          greens++
+        }
+        if (word.charAt(i) === l) {
+          countInWord++;
+        }
+        if (letters[i].toLowerCase() === l) {
+          countInLetters++;
+        }
+      }
+      return Math.min(countInWord-greens,countInLetters-greens)
+    }
+    let isYellow = (word, letters, totalYellows, index) => {
+      let l = letters[index].toLowerCase();
+      let lBefore = 0;
+      for (let i = 0; i < index; i++) {
+        if (letters[i].toLowerCase() === l && word.charAt(i) !== l) {
+          lBefore++;
+        }
+      }
+      console.log(letters, totalYellows, index, lBefore)
+      return totalYellows > lBefore;
+    }
     return (
         <div className="Line">
             {
                 letters.map((l, i) => {
-                    return <Square key={i} isSelected={isActive && selectedSquare === i} letter={letters[i]} word={word} position={i} done={done} handleSquareClick={handleSquareClick} />
+                    l = l.toLowerCase();
+                    let color = ""
+                    if (word.charAt(i) === l) {
+                      color = " green"
+                    } else if (word.includes(l)) {
+                      let totalYellows = countYellows(word, letters, l);
+                      color = isYellow(word, letters, totalYellows, i) ? " yellow" : ""
+                    }
+
+                    return <Square key={i} isSelected={isActive && selectedSquare === i} letter={letters[i]} word={word} position={i} done={done} handleSquareClick={handleSquareClick} colorClass={color}/>
 
                 })
             }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,15 @@
-function Square({isSelected, letter, word, position, done, handleSquareClick}) {
+function Square({isSelected, letter, word, position, done, handleSquareClick, colorClass}) {
     let classes = isSelected ? "Square selected" : "Square"
 
-    if(done && letter.toLowerCase() === word.charAt(position)) {
-        classes += ' green'
-    } else if (done && letter && letter.toLowerCase() !== word.charAt(position) && word.includes(letter.toLowerCase())) {
-        classes += ' yellow'
+    if (done) {
+      classes += colorClass
     }
+
+    // if(done && letter.toLowerCase() === word.charAt(position)) {
+    //     classes += ' green'
+    // } else if (done && letter && letter.toLowerCase() !== word.charAt(position) && word.includes(letter.toLowerCase())) {
+    //     classes += ' yellow'
+    // }
 
     return (
       <div className={classes} onClick={()=>handleSquareClick(position)}>
